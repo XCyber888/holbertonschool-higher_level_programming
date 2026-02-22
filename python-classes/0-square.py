91bf994b13676e22824af1808c5c52688a80d9b5
XCyber888 <xalidyusifov@gmail.com>
2026-02-22 14:18:15 +0000

    Add empty Square class with docs

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module defines an empty Square class.
+
+This module is part of a series of exercises to learn OOP.
+"""
+
+
+class Square:
+    """
+    An empty class that defines a square.
+    """
+    pass
