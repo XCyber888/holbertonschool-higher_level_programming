07356d682ce6630aca9c074ecb27b3493aa927bd
XCyber888 <xalidyusifov@gmail.com>
2026-02-22 14:33:50 +0000

    Add empty Square class with mandatory long docstrings

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-This module provides a base definition for a Square class.
+This module provides a basic definition of a Square class.
 
-This is part of the Python - Classes and Objects project.
+The Square class is a fundamental part of this project, aimed at
+demonstrating the basics of Object-Oriented Programming in Python.
+This module does not perform any complex operations yet.
 """
 
 
 class Square:
     """
-    An empty class that defines a square.
+    A class used to represent a Square.
+
+    The Square class is currently empty to satisfy the requirements
+    of the first task in the Python classes and objects project.
     """
     pass
