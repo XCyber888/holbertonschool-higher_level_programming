f650a10bb225b2ab90a5f72812751e940ca87a8f
XCyber888 <xalidyusifov744@gmail.com>
2026-03-04 14:59:24 +0000

    Fix: Added README and improved documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-"""Module for reading a file."""
+"""
+This module contains a function that reads a text file.
+"""
 
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
