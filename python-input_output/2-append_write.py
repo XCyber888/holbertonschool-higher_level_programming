af441a8de7ad4abf15985707b97afdf7e2ecc4d7
XCyber888 <xalidyusifov744@gmail.com>
2026-03-04 15:02:23 +0000

    Task 2: Append to a file implementation

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains a function that appends a string to a text file.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string to a text file (UTF8) and returns the number of characters.
+
+    Args:
+        filename (str): The name of the file.
+        text (str): The text to append.
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
