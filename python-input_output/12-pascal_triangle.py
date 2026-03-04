8e5fb5794afb3b81f7441d294e0ef2d17f7a8835
XCyber888 <xalidyusifov744@gmail.com>
2026-03-04 15:58:35 +0000

    Task 12: Pascal Triangle implementation

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
-Module for Pascal Triangle.
+Module for Pascal Triangle function.
 """
 
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal triangle of n.
+    Returns a list of lists of integers representing Pascal triangle of n.
     """
     if n <= 0:
         return []
 
     triangle = [[1]]
-    while len(triangle) < n:
-        prev_row = triangle[-1]
-        new_row = [1]
-        for i in range(len(prev_row) - 1):
-            new_row.append(prev_row[i] + prev_row[i + 1])
-        new_row.append(1)
-        triangle.append(new_row)
+    for i in range(1, n):
+        prev = triangle[-1]
+        row = [1]
+        for j in range(len(prev) - 1):
+            row.append(prev[j] + prev[j + 1])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
