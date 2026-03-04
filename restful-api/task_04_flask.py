cfac73c11161a9773d4b898dd549709132250f59
XCyber888 <xalidyusifov744@gmail.com>
2026-03-04 16:42:17 +0000

    Fix Task 4: Change duplicate user error code to 409

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -51,7 +51,7 @@ def add_user():
         return jsonify({"error": "Username is required"}), 400
     
     if username in users:
-        return jsonify({"error": "User already exists"}), 400
+        return jsonify({"error": "Username already exists"}), 409
     
     users[username] = data
     return jsonify({"message": "User added", "user": data}), 201
