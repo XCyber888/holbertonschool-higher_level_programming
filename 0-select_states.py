17a5ea67017f972c21c861ae46291beeec156d89
XCyber888 <xalidyusifov744@gmail.com>
2026-03-12 15:34:04 +0000

    Fix: Task 0 PEP 8 and formatting

--- 0-select_states.py
+++ 0-select_states.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3])
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create a cursor object to execute queries
     cur = db.cursor()
+
+    # Execute the SQL query
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
+
+    # Fetch all the results
+    query_rows = cur.fetchall()
+
+    # Print each row in the required format
+    for row in query_rows:
         print(row)
+
+    # Close the cursor and the database connection
     cur.close()
     db.close()
