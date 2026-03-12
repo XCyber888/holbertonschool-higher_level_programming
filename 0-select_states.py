699d746128e675e6ceff5de4bdd520a58e97cf22
XCyber888 <xalidyusifov744@gmail.com>
2026-03-12 15:34:27 +0000

    Final Task 0 submission with correct branch and formatting

--- 0-select_states.py
+++ 0-select_states.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
-"""
+"""Lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # Connect to the MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
-    # Create a cursor object to execute queries
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
-
-    # Execute the SQL query
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all the results
-    query_rows = cur.fetchall()
-
-    # Print each row in the required format
-    for row in query_rows:
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    # Close the cursor and the database connection
     cur.close()
     db.close()
