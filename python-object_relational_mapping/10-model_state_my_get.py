8c704f8d8df86935fe478aae8cf7c4033d6ee022
Xalid Yusifov <xalid.yusifov@holberton.com>
2026-03-14 15:23:19 +0000

    Task 10: Full rewrite for 10/10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -14,9 +14,8 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    # .first() istifadə edirik və binary/literal müqayisəni SQLAlchemy-yə buraxırıq
     state = session.query(State).filter(State.name == sys.argv[4]).first()
-    if state:
+    if state is not None:
         print("{}".format(state.id))
     else:
         print("Not found")
