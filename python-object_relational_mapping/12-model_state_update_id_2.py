104295d9ae4ba5048d5d34e0d4c7e8bb6c7f3bbd
Xalid Yusifov <xalid.yusifov@holberton.com>
2026-03-14 15:17:10 +0000

    Add task 12: Update state name where id = 2

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Changes the name of a State object from the database hbtn_0e_6_usa.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    # Engine və Session yaradılması
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # ID-si 2 olan ştatı tapırıq
+    state_to_update = session.query(State).filter(State.id == 2).first()
+
+    # Əgər tapılsa, adını dəyişib commit edirik
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+
+    session.close()
