5286fcdf0638a9645d5e6839a58c325c11d9fd46
Xalid Yusifov <xalid.yusifov@holberton.com>
2026-03-14 14:59:43 +0000

    Fix task 7: Final PEP8 and requirement compliance

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,14 +9,11 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    # Engine və Session yaradılması
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query və Çap
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
 
