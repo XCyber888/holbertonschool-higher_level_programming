4b7d13de5f838f9e8b297aea7a40fe72b18acf7d
Xalid Yusifov <xalid.yusifov@holberton.com>
2026-03-14 15:26:53 +0000

    Task 6: Definitive model_state for 10/10

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a State and an instance Base = declarative_base()
+Contains the class definition of a State and an instance Base
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,9 +10,9 @@ Base = declarative_base()
 
 class State(Base):
     """
-    State class inherits from Base and links to the MySQL table states
+    State class inherits from Base and links to MySQL table states
     """
-    __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, unique=True,
+    __tablename__ = "states"
+    id = Column(Integer, autoincrement=True, unique=True, 
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
