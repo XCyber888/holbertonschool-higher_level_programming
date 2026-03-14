7888679f5db5739734458086895fc6242ddfe56d
Xalid Yusifov <xalid.yusifov@holberton.com>
2026-03-14 15:02:33 +0000

    Fix task 6: Standardized model_state for checker

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -15,5 +15,5 @@ class State(Base):
     - links to the MySQL table states
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
