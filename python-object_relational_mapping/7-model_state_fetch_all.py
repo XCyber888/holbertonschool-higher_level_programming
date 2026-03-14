4f302a4d6ec272239ad8fac62c10010885220f41
Xalid Yusifov <xalid.yusifov@holberton.com>
2026-03-14 14:55:38 +0000

    Fix task 7: Adjust import order and formatting

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,27 +3,21 @@
 Lists all State objects from the database hbtn_0e_6_usa.
 """
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 
 if __name__ == "__main__":
-    # Engine yaradırıq
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # Engine və Session yaradılması
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     
-    # Session klassını yaradırıq və engine-ə bağlayırıq
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Bütün State-ləri id-yə görə sıralayıb çəkirik
-    states = session.query(State).order_by(State.id).all()
-
-    # Nəticələri çap edirik
-    for state in states:
+    # Query və Çap
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
 
-    # Session-ı bağlayırıq
     session.close()
