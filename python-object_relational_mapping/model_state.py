674ae9fbe7d2beac64f79c442461023179ee0700
Xalid Yusifov <xalid.yusifov@holberton.com>
2026-03-14 15:04:10 +0000

    Task 6: Explicitly adding all requirements for the checker

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,5 +13,6 @@ class State(Base):
     State class inherits from Base and links to the MySQL table states
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
