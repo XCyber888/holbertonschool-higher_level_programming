52d05abb820eb43f008428874af1dbe972e57b95
Xalid Yusifov <xalid.yusifov@holberton.com>
2026-03-14 15:22:05 +0000

    Fix task 10: Perfect formatting and None check

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""
-Prints the State object with the name passed as argument
-from the database hbtn_0e_6_usa.
-"""
+"""Lists the State object with the name passed as argument"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    # Engine yaradılır
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-
-    # Session konfiqurasiyası
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Bazadan axtarış (SQL Injection-dan tam qorunmaq üçün filter istifadəsi)
     state = session.query(State).filter(State.name == sys.argv[4]).first()
-
-    # Nəticənin çıxarılması
     if state is not None:
         print("{}".format(state.id))
     else:
         print("Not found")
-
     session.close()
