8ee963f1edaccc2d42427353d4614086bd6f701e
Xalid Yusifov <xalid.yusifov@holberton.com>
2026-03-14 14:53:09 +0000

    Fix: model_state.py PEP8 and requirements

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,19 +5,15 @@ Contains the class definition of a State and an instance Base = declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# Base obyektini yaradırıq, bütün klasslar bundan miras alacaq
 Base = declarative_base()
 
 
 class State(Base):
     """
-    State class inherits from Base and links to the MySQL table states
+    State class:
+    - inherits from Base
+    - links to the MySQL table states
     """
     __tablename__ = 'states'
-    
-    # id sütunu: auto-generated, unique integer, not null, primary key
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                nullable=False, unique=True)
-    
-    # name sütunu: string max 128 characters, not null
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
